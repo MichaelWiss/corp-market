b8515cd9191a43695794100dde0d20fa85fae949
MichaelWiss <michael.wiss@gmail.com>
2024-10-02 15:45:54 -0500

    image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
-
-
+import Image from "next/image";
+import homeImg from 'public/home.jpg';
 
 export default function Home() {
   return <div>
     
     Home Page
+    <Image src={homeImg} alt="car factory"></Image>
     </div>;
 };
