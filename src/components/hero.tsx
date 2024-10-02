5cbcda838e8da5d0104d8fbca6f751eee308ed85
MichaelWiss <michael.wiss@gmail.com>
2024-10-02 17:24:07 -0500

    props

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -8,7 +8,23 @@ interface HeroProps {
 }
 
 const Hero = (props: HeroProps) => {
-    return (  );
+    return ( 
+        <div className="relative h-screen">
+            <div className="absolute -z-10 inset-0">
+                <Image 
+                    src={props.imgData} 
+                    alt="props.imgAlt"
+                    fill 
+                    style={{ objectFit:'cover' }}
+                />
+            </div>
+            <div className="pt-48 flex-justify-center items-center">
+                <h1 className="text-white text-6xl">
+                    {props.title}
+                </h1>
+            </div>
+        </div>
+     );
 }
  
 export default Hero;
